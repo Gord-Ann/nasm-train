1dc1a1c124ebf80ab0bf422b208ac48f67369522
Lampirg <lampirg1@gmail.com>
2023-02-25 19:50:01 +0300

    Add atoi function Not working

--- math.asm
+++ math.asm
@@ -9,23 +9,29 @@ SECTION .data
     newLineLen:  EQU $-newLine
 SECTION .bss
     x resb 1
+    converted resb 1
     y resb 1
 
 SECTION .text
     GLOBAL _start 
 
 _start:
-    ; mov eax, SYS_READ ; read
-    ; mov ebx, STDIN
-    ; mov ecx, x 
-    ; mov edx, 10
-    ; int 0x80
+    mov eax, SYS_READ ; read
+    mov ebx, STDIN
+    mov ecx, x 
+    mov edx, 10
+    int 0x80
+    
+    mov edx, [x]
+    call atoi
+    add eax, 10
+    mov [y], eax
 
     mov eax, 12
     mov ebx, 2
     mul ebx
     add eax, '0'
-    mov [y], eax
+    ; mov [y], eax
 
     mov eax, SYS_WRITE ; write
     mov ebx, STDOUT
@@ -41,3 +47,19 @@ _start:
     mov eax, SYS_EXIT
     mov ebx, 0
     int 80h
+
+atoi:
+    xor eax, eax ; zero a "result so far"
+    .top:
+    movzx ecx, byte [edx] ; get a character
+    inc edx ; ready for next one
+    cmp ecx, '0' ; valid?
+    jb .done
+    cmp ecx, '9'
+    ja .done
+    sub ecx, '0' ; "convert" character to number
+    imul eax, 10 ; multiply "result so far" by ten
+    add eax, ecx ; add in current digit
+    jmp .top ; until done
+.done:
+    ret
