42b255180b59f718083b32a87874738c4916b891
Mikhail <57584673+Klementor@users.noreply.github.com>
2023-03-10 21:25:31 +0300

    Create lab2_Krylov_Mikhail.asm

---
+++ lab2_Krylov_Mikhail.asm
@@ -0,0 +1,120 @@
+SYS_EXIT EQU 1
+SYS_READ EQU 3
+SYS_WRITE EQU 4
+STDIN EQU 0
+STDOUT EQU 1
+
+SECTION .data
+
+      msg1 DB "Enter x for 6 var", 0xA,0xD
+      len1 EQU $- msg1
+
+      msg2 DB "The answer is: "
+      len2 EQU $- msg2
+
+      next_line DB 0xA,0xD
+      len3 EQU $- next_line
+
+SECTION .bss
+
+      num RESB 1
+      res RESB 1
+
+SECTION .text
+      global _start
+
+_start:
+; printing: "Enter x for 6 var"
+      MOV eax, SYS_WRITE
+      MOV ebx, STDOUT
+      MOV ecx, msg1
+      MOV edx, len1
+      INT 0x80
+; reading a x into num variable
+      MOV eax, SYS_READ
+      MOV ebx, STDIN
+      MOV ecx, num
+      MOV edx, 2
+      INT 0x80
+; converting input string to int
+      LEA edx, [num]
+      CALL string_to_int
+      MOV [num], eax
+; printing: "The answer is: "
+      MOV eax, SYS_WRITE
+      MOV ebx, STDOUT
+      MOV ecx, msg2
+      MOV edx, len2
+      INT 0x80
+; calculating
+      MOV eax, 0
+      MOV ebx, 0
+      MOV eax, 5
+      MOV ebx, 5
+      MUL ebx
+      MOV ebx, [num]
+      MUL ebx
+      MOV ecx, 0
+      MOV ecx, eax
+      MOV eax, 33
+      MOV ebx, [num]
+      MOV edx, 0
+      DIV ebx
+      ADD eax, ecx
+      MOV ebx, 5
+      ADD ebx, 5
+      MOV edx, 0
+      DIV ebx
+; converting int to string
+      MOV [res],eax
+      MOV [res],byte 0
+      LEA esi,[res]
+      CALL int_to_string
+; printing the answer
+      MOV eax, SYS_WRITE
+      MOV ebx, STDOUT
+      MOV ecx, res
+      MOV edx, 10
+      INT 0x80
+; printing the endline symbols
+      MOV eax, SYS_WRITE
+      MOV ebx, STDOUT
+      MOV ecx, next_line
+      MOV edx, len3
+      INT 0x80
+
+exit:
+      MOV eax, SYS_EXIT
+      XOR ebx, ebx
+      INT 0x80
+
+string_to_int:
+      XOR eax, eax
+.top:
+      MOVZX ecx, byte [edx]
+      INC edx
+      CMP ecx, '0'
+      JB .done
+      CMP ecx, '9'
+      JA .done
+      SUB ecx, '0'
+      IMUL eax, 10
+      ADD eax, ecx
+      JMP .top
+      .done:
+      RET
+
+int_to_string:
+      ADD esi, 9
+      MOV byte [esi], 0
+      MOV ebx,10
+.next_digit:
+      XOR edx, edx ; Clear edx prior to dividing edx:eax by ebx
+      DIV ebx ; eax /= 10
+      ADD dl,'0' ; Convert the remainder to ASCII
+      DEC esi ; store characters in reverse order
+      MOV [esi], dl
+      TEST eax, eax
+      JNZ .next_digit ; Repeat until eax==0
+      MOV eax, esi
+      RET
